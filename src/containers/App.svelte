b87fc71f24451222fd38b643ea461b03f1c6ad55
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-09-11 21:54:06 -0500

    Curso Svelte Platsi --Pugstagram-- 😍

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,13 +1,30 @@
 <script>
+  import {onMount} from 'svelte'
+
   import Header from '../components/Header.svelte'
   import Main from '../components/Main.svelte'
   import TimeLine from '../components/TimeLine.svelte'
   import Sidebar from '../components/Sidebar.svelte'
+import Card from '../components/Card.svelte'
+
+  let data = {}
+  const API = 'https://us-central1-pugstagram-co.cloudfunctions.net/data'
+  const user = {
+    nickname: 'ByRedHunter',
+    name: 'Jhonny Quispe'
+  }
+
+  onMount(async () => {
+    const response = await fetch(API)
+    data = await response.json()
+  })
 </script>
 
 <Header />
-<Main><TimeLine />
-<Sidebar /></Main>
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...user} />
+</Main>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
